b105df17e863c4f239fdb0f4254831091cecbeae
iarosch <vit.iarosch@yandex.by>
2025-02-11 10:59:34 +0300

    feat: add artwork item layout for recycler view

--- ArticApp/app/build.gradle.kts
+++ ArticApp/app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.hilt.android)
+    implementation(libs.androidx.constraintlayout)
     kapt(libs.hilt.compiler)
 }
 
